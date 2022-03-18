3aceec426bb8555827fda1085f247e47e33a581d
kevindaspam@gmail.com <kevindaspam@gmail.com>
2022-03-18 11:09:57 -0500

    v.1.4.2 Implemented flattenObject Implemented unflattenObject

--- index.js
+++ index.js
@@ -128,14 +128,96 @@ const chooseWeightedRandomWithWeightKey = (input) => {
 };
 module.exports.chooseWeightedRandomWithWeightKey = chooseWeightedRandomWithWeightKey;
 
-// console.log(
-// 	chooseRandomWeightedObject({
-// 		one: 1,
-// 		two: 2,
-// 		three: 3,
-// 	})
-// );
-const chooseRandomWeightedObject = (allObjects) => {
+// https://stackoverflow.com/questions/19098797/fastest-way-to-flatten-un-flatten-nested-json-objects
+const flattenObject = (function (isArray, wrapped) {
+	return function (table) {
+		return reduce('', {}, table);
+	};
+
+	function reduce(path, accumulator, table) {
+		if (isArray(table)) {
+			var length = table.length;
+
+			if (length) {
+				var index = 0;
+
+				while (index < length) {
+					var property = path + '[' + index + ']',
+						item = table[index++];
+					if (wrapped(item) !== item) accumulator[property] = item;
+					else reduce(property, accumulator, item);
+				}
+			} else accumulator[path] = table;
+		} else {
+			var empty = true;
+
+			if (path) {
+				for (var property in table) {
+					var item = table[property],
+						property = path + '.' + property,
+						empty = false;
+					if (wrapped(item) !== item) accumulator[property] = item;
+					else reduce(property, accumulator, item);
+				}
+			} else {
+				for (var property in table) {
+					var item = table[property],
+						empty = false;
+					if (wrapped(item) !== item) accumulator[property] = item;
+					else reduce(property, accumulator, item);
+				}
+			}
+
+			if (empty) accumulator[path] = table;
+		}
+
+		return accumulator;
+	}
+})(Array.isArray, Object);
+module.exports.flattenObject = flattenObject;
+
+// https://stackoverflow.com/questions/19098797/fastest-way-to-flatten-un-flatten-nested-json-objects
+const unflattenObject = (table) => {
+	var result = {};
+
+	for (var path in table) {
+		var cursor = result,
+			length = path.length,
+			property = '',
+			index = 0;
+
+		while (index < length) {
+			var char = path.charAt(index);
+
+			if (char === '[') {
+				var start = index + 1,
+					end = path.indexOf(']', start),
+					cursor = (cursor[property] = cursor[property] || []),
+					property = path.slice(start, end),
+					index = end + 1;
+			} else {
+				var cursor = (cursor[property] = cursor[property] || {}),
+					start = char === '.' ? index + 1 : index,
+					bracket = path.indexOf('[', start),
+					dot = path.indexOf('.', start);
+
+				if (bracket < 0 && dot < 0) var end = (index = length);
+				else if (bracket < 0) var end = (index = dot);
+				else if (dot < 0) var end = (index = bracket);
+				else var end = (index = bracket < dot ? bracket : dot);
+
+				var property = path.slice(start, end);
+			}
+		}
+
+		cursor[property] = table[path];
+	}
+
+	return result[''];
+};
+module.exports.unflattenObject = unflattenObject;
+
+const chooseRandomWeightedObject = (allObjects, recursive = false) => {
 	const totalWeight = Object.values(allObjects).reduce((acc, currentWeight) => acc + currentWeight, 0);
 
 	let currentWeight = Math.floor(Math.random() * totalWeight);
