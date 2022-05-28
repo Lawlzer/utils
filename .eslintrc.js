771298801aa44e77ec6f76f00012d332417ba005
kevindaspam@gmail.com <kevindaspam@gmail.com>
2022-05-28 09:01:35 -0500

    Update configuration files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,25 @@
 module.exports = {
-    "parserOptions": {
-        "ecmaVersion": 11,
-        "sourceType": "module",
-        "ecmaFeatures": {
-            "jsx": true
-        }
-    },
-    "rules": {
-        "semi": 2,
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "indent": [
-            "error",
-            "tab"
-        ]
-    },
-    "env": {
-        "node": true
-    },
-
-    "parser": "@typescript-eslint/parser",
-    // "tsconfigRootDir": "C:\\Users\\Kevin\\Desktop\\Nerd Stuff\\Typescrippppptttt"
-    "tsconfigRootDir": __dirname,
+	'parserOptions': {
+		'tsconfigRootDir': __dirname,
+		'ecmaVersion': 11,
+		'sourceType': 'module',
+		'ecmaFeatures': {
+			'jsx': true
+		}
+	},
+	'rules': {
+		'semi': [2, 'always'],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'indent': [
+			'error',
+			'tab'
+		]
+	},
+	'env': {
+		'node': true
+	},
+	'parser': '@typescript-eslint/parser',
 };
