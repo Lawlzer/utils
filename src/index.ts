62693043a22f71f117130bd653753edc3699b5af
kevindaspam@gmail.com <kevindaspam@gmail.com>
2022-05-28 09:02:07 -0500

    v2.0.4 release Implement objectMap

--- src/index.ts
+++ src/index.ts
@@ -437,3 +437,24 @@ export function keys<O extends object>(obj: O): Array<keyof O> {
 };
 
 
+
+// https://stackoverflow.com/questions/14810506/map-function-for-objects-instead-of-arrays
+interface Dictionary<T> {
+    [key: string]: T;
+}
+export function objectMap<TValue, TResult>(
+	obj: Dictionary<TValue>,
+	valSelector: (val: TValue, obj: Dictionary<TValue>) => TResult,
+	keySelector?: (key: string, obj: Dictionary<TValue>) => string,
+	ctx?: Dictionary<TValue>
+) {
+	const ret = {} as Dictionary<TResult>;
+	for (const key of Object.keys(obj)) {
+		const retKey = keySelector
+			? keySelector.call(ctx || null, key, obj)
+			: key;
+		const retVal = valSelector.call(ctx || null, obj[key], obj);
+		ret[retKey] = retVal;
+	}
+	return ret;
+}
