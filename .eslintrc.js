6367335ad6cad214ecc49bac9974d4f2e7482e02
Lawlzer <kevindaspam@gmail.com>
2022-06-28 14:25:55 -0500

    v2.0.7 Update package.json (npm run publish) New .eslintrc.js New .eslintignore Implement fileExists

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,54 @@
 module.exports = {
-	'parserOptions': {
-		'tsconfigRootDir': __dirname,
-		'ecmaVersion': 11,
-		'sourceType': 'module',
-		'ecmaFeatures': {
-			'jsx': true
-		}
-	},
+	'parser': '@typescript-eslint/parser',
+	'plugins': ['@typescript-eslint'],
 	'rules': {
 		'semi': [2, 'always'],
 		'quotes': [
 			'error',
 			'single'
 		],
-		'indent': [
+		'indent': 2,
+		'no-trailing-spaces': 'off',
+		'no-unused-vars': 'off',
+		'no-extra-semi': 'off',
+		'no-explicit-any': 'off',
+		'no-var': 'off',
+		'no-prototype-builtins': 'off',
+		'no-useless-escape': 'off',
+		'linebreak-style': 'off',
+
+		'@typescript-eslint/no-unused-vars': 'off',
+		'@typescript-eslint/no-extra-semi': 'off',
+		'@typescript-eslint/no-trailing-spaces': 'off',
+		'@typescript-eslint/no-explicit-any': 'off',
+
+		'@typescript-eslint/no-var-requires': 'off',
+
+		'@typescript-eslint/no-misused-promises': [
 			'error',
-			'tab'
-		]
+			{
+				'checksVoidReturn': false
+			},
+		],
+		'strict': 2,
+
+		// 'no-console': 2 // Disables all console.logs(), ONLY UNCOMMENT FOR TESTING ESLINT.
 	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+	],
+	'parserOptions': {
+		'tsconfigRootDir': __dirname,
+		'ecmaVersion': 2020,
+		'sourceType': 'module',
+		'ecmaFeatures': {
+			'jsx': true
+		},
+		'project': ['./tsconfig.json', './src/**/*.ts'],
+	},
+
 	'env': {
 		'node': true
 	},
-	'parser': '@typescript-eslint/parser',
 };
