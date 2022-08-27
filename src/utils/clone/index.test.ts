6ed8066d2f640e8ebadd27e4c93ea587b358de07
Lawlzer <Kevindaspam@gmail.com>
2022-08-27 17:06:02 -0500

    ✨Implement clone  & clone tests

--- src/utils/clone/index.test.ts
+++ src/utils/clone/index.test.ts
@@ -1,89 +1,32 @@
 import { clone } from './index';
 const folderName = __dirname.split('\\').pop()!;
 
-// TODO
 describe(folderName, () => {
-	it.todo('will return a copy of an object');
-	it.todo('will not be the exact same as the input object');
-	it.todo('will clone nested objects');
-	it.todo('will clone nested arrays');
-	it.todo('will work with Dates');
-	it.todo('will work with RegExps');
-	it.todo('will work with Maps');
-	it.todo('will work with Sets');
-	it.todo('will work with Functions');
-	it.todo('will work with cyclic references');
-	it.todo('will work for complex nested arrays & objects');
+	it('will clone an array', () => {
+		const input = [1, 2, 3];
+		const output = clone(input);
+		expect(output).toEqual(input);
+		expect(output).not.toBe(input);
+	});
 
-	// it('will return a copy of an object', async () => {
-	// 	const object = { a: 1, b: 2, c: 3 };
-	// 	const copy = cloneShallow(object);
-	// 	expect(copy).toEqual(object);
-	// });
+	it('will clone an object', () => {
+		const input = { a: 1, b: 2, c: 3 };
+		const output = clone(input);
+		expect(output).toEqual(input);
+		expect(output).not.toBe(input);
+	});
 
-	// it('will not be the exact same as the input object', async () => {
-	// 	const object = { a: 1, b: 2, c: 3 };
-	// 	const copy = cloneShallow(object);
-	// 	expect(copy).not.toBe(object);
-	// });
+	it('will clone an object with nested arrays', () => {
+		const input = { a: 1, b: 2, c: [1, 2, 3] };
+		const output = clone(input);
+		expect(output).toEqual(input);
+		expect(output).not.toBe(input);
+	});
 
-	// it('will clone nested objects', async () => {
-	// 	const object = { a: 1, b: 2, c: { d: 3, e: 4 } };
-	// 	const copy = cloneShallow(object);
-	// 	expect(copy).toEqual(object);
-	// 	expect(copy.c).not.toBe(object.c);
-	// });
-
-	// it('will clone arrays', async () => {
-	// 	const object = [1, 2, 3];
-	// 	const copy = cloneShallow(object);
-	// 	expect(copy).toEqual(object);
-	// 	expect(copy).not.toBe(object);
-	// });
-
-	// it('will clone nested arrays', async () => {
-	// 	const object = [1, 2, [3, 4]];
-	// 	const copy = cloneShallow(object);
-	// 	expect(copy).toEqual(object);
-	// 	expect(copy).not.toBe(object);
-	// 	expect(copy[2]).not.toBe(object[2]);
-	// });
-
-	// it('will clone nested arrays with nested objects', async () => {
-	// 	const object = [1, 2, [3, 4, { a: 1, b: 2, c: { d: 3, e: 4, f: [5, 6, 7] } }]];
-	// 	const copy = cloneShallow(object);
-	// 	expect(copy).toEqual(object);
-	// 	expect(copy).not.toBe(object);
-	// });
-
-	// it('will store Regular Expressions', async () => {
-	// 	const object = /^[a-zA-Z0-9]+$/;
-	// 	const copy = cloneShallow(object);
-	// 	expect(copy).toEqual(object);
-	// 	expect(copy).not.toBe(object);
-	// });
-
-	// it('will store Date objects', async () => {
-	// 	const object = new Date();
-	// 	const copy = cloneShallow(object);
-	// 	expect(copy).toEqual(object);
-	// 	expect(copy).not.toBe(object);
-	// });
-
-	// it('will store Functions', async () => {
-	// 	const object = function () {};
-	// 	const copy = cloneShallow(object);
-	// 	expect(copy).toEqual(object);
-	// 	expect(copy).not.toBe(object);
-	// });
-
-	// it('will work with cyclic references', async () => {
-	// 	const object = { a: 1, b: 2, c: { d: 3, e: 4 } };
-	// 	object.c.f = object;
-	// 	const copy = cloneShallow(object);
-	// 	expect(copy).toEqual(object);
-	// 	expect(copy).not.toBe(object);
-	// 	expect(copy.c).not.toBe(object.c);
-	// 	expect(copy.c.f).not.toBe(object.c.f);
-	// });
+	it('will clone an object with nested objects', () => {
+		const input = { a: 1, b: 2, c: { a: 1, b: 2, c: 3 } };
+		const output = clone(input);
+		expect(output).toEqual(input);
+		expect(output).not.toBe(input);
+	});
 });
--- src/utils/clone/index.ts
+++ src/utils/clone/index.ts
@@ -1,9 +1,23 @@
+import { objectMap } from '~/utils/objectMap';
+
 /**
- * DATEs are not yet supported
- * Regular Expressions are not yet supported
- * Functions are not yet supported
+ * Supports nested arrays and objects.
+ *
+ * Does not support complex object types: Dates, RegExps, Functions, etc.
  */
-export function clone<T>(object: T): T {
-	throw new Error('@lawlzer/helpers - clone - this function is not yet implemented.');
-	return JSON.parse(JSON.stringify(object));
+export function clone<T>(input: T): T {
+	const isArray = Array.isArray(input);
+	if (isArray)
+		return input.map((item) => {
+			return clone(item);
+		}) as any;
+
+	const isObject = typeof input === 'object' && input !== null;
+	if (isObject) {
+		const output = objectMap(input as any, (value) => {
+			return clone(value);
+		});
+		return output as any;
+	}
+	return input;
 }
