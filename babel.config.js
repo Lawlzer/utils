01f55bcf86b1381c2b1219b9385827d804194974
Lawlzer <Kevindaspam@gmail.com>
2022-08-29 12:35:22 -0500

    🐛Re-implement Paths for Babel (tsc-alias only touches .d.ts file paths, not .js file paths)

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,18 @@
 module.exports = {
 	presets: ['@babel/preset-env', '@babel/preset-typescript'],
-	plugins: [],
+	plugins: [
+		['@babel/plugin-transform-runtime'], // Fixes a regeneratorRuntime issue
+		// ['@babel/preset-env', { targets: { node: 'current' } }],
+		[
+			'module-resolver',
+			{
+				root: ['.'],
+				alias: {
+					'~': './dist',
+					// '@test-utils': './test-utils',
+					'@test-utils': './test-utils',
+				},
+			},
+		],
+	],
 };
