e1e16f45672951b20618192cefe6b17b63c2885d
Lawlzer <Kevindaspam@gmail.com>
2022-08-29 12:25:43 -0500

    🔧Now using absolute imports (again) Updated to replace absolute -> relative imports post-build

--- babel.config.js
+++ babel.config.js
@@ -1,26 +1,4 @@
 module.exports = {
 	presets: ['@babel/preset-env', '@babel/preset-typescript'],
-	plugins: [
-		[
-			'babel-plugin-root-import',
-			{
-				paths: [
-					{
-						rootPathSuffix: './src',
-						rootPathPrefix: '~/',
-					},
-				],
-			},
-		],
-		// [
-		// 	'module-resolver',
-		// 	{
-		// 		root: ['.'],
-		// 		alias: {
-		// 			'~': './dist',
-		// 			'@test-utils': './test-utils',
-		// 		},
-		// 	},
-		// ],
-	],
+	plugins: [],
 };
--- src/index.ts
+++ src/index.ts
@@ -3,28 +3,31 @@
 // Ideally, we would dynamically export every function without referencing them here, but that seemed to be impossible.
 // If you know a better way to do this with TypeScript (that offers intellisense), I'd love to know :p
 
-export * from './utils/clamp'; // If I do this (absolute import from src/dist), there is no intellisense.
-export * from './utils/clone';
-export * from './utils/deepCompare';
-export * from './utils/ensureExists';
-export * from './utils/flatten';
-export * from './utils/floorToPrecision';
-export * from './utils/getAllFiles'; // If I do this (relative import), there is intellisense :/
-export * from './utils/getAmountOfTimesInArray';
-export * from './utils/getRandomCharacters';
-export * from './utils/getUniques';
-export * from './utils/keys';
-export * from './utils/lowerCaseObjectKeys';
-export * from './utils/multiSort';
-export * from './utils/objectHasKeysWithType';
-export * from './utils/objectMap';
-export * from './utils/pluck';
-export * from './utils/racePromises';
-export * from './utils/replaceAll';
-export * from './utils/returnRandomElement';
-export * from './utils/returnRandomWeightedObject';
-export * from './utils/shuffleArray';
-export * from './utils/sleep';
-export * from './utils/temporarySave';
-export * from './utils/types';
-export * from './utils/UwU';
+// Secondly, it would be great to use absolute imports at all, but I haven't found a package that will modify the *.d.ts file absolute imports.
+// If we have .d.ts files with absolute imports, BUILDING other projects (that use this package) will error!
+
+export * from '~/utils/clamp'; // If I do this (absolute import from src/dist), there is no intellisense.
+export * from '~/utils/clone';
+export * from '~/utils/deepCompare';
+export * from '~/utils/ensureExists';
+export * from '~/utils/flatten';
+export * from '~/utils/floorToPrecision';
+export * from '~/utils/getAllFiles'; // If I do this (relative import), there is intellisense :/
+export * from '~/utils/getAmountOfTimesInArray';
+export * from '~/utils/getRandomCharacters';
+export * from '~/utils/getUniques';
+export * from '~/utils/keys';
+export * from '~/utils/lowerCaseObjectKeys';
+export * from '~/utils/multiSort';
+export * from '~/utils/objectHasKeysWithType';
+export * from '~/utils/objectMap';
+export * from '~/utils/pluck';
+export * from '~/utils/racePromises';
+export * from '~/utils/replaceAll';
+export * from '~/utils/returnRandomElement';
+export * from '~/utils/returnRandomWeightedObject';
+export * from '~/utils/shuffleArray';
+export * from '~/utils/sleep';
+export * from '~/utils/temporarySave';
+export * from '~/utils/types';
+export * from '~/utils/UwU';
