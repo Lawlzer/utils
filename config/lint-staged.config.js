0d7b8e6cad30794ba9e92d22f90016da581d2032
Lawlzer <Kevindaspam@gmail.com>
2022-09-21 19:51:02 -0500

    🧑‍💻Move Jest from pre-commit to lint-staged This fixes a bug with commiting with Jest errors, and improves Commit performance

--- config/lint-staged.config.js
+++ config/lint-staged.config.js
@@ -1,5 +1,5 @@
 // We don't want to use the global "npm run lint" because those run on EVERY file. We want to run on commited files, only.
 
 module.exports = {
-	'**/*.{ts,tsx,js,jsx,json,jsonc}': ['eslint --fix --ignore-path ./.gitignore --config ./config/.eslintrc.js', 'prettier --write --config ./config/.prettierrc.js'],
+	'**/*.{ts,tsx,js,jsx,json,jsonc}': ['eslint --fix --ignore-path ./.gitignore --config ./config/.eslintrc.js', 'prettier --write --config ./config/.prettierrc.js', 'jest --bail --findRelatedTests --pass-with-no-tests --config=./config/jest.config.ts'],
 };
