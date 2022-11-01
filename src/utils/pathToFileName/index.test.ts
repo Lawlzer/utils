4d0fa7d24909e2109c8009045c98fe41777e3f5b
Kevin Derp <kevindaspam@gmail.com>
2022-11-01 12:37:08 -0500

    🧵Remove unnecessary async from pathToFileName Add new test (for 100% branch coverage) to pathToFileName v3.2.7 release

--- src/utils/pathToFileName/index.test.ts
+++ src/utils/pathToFileName/index.test.ts
@@ -7,32 +7,38 @@ const folderName = __dirname.split('\\').pop()!;
 
 describe(folderName, () => {
 	it('will work with a filename', async () => {
-		const fileName = await pathToFileName('test.txt');
+		const fileName = pathToFileName('test.txt');
 		expect(fileName).toBe('test.txt');
 	});
 
 	it('will work with a /path/to/filename', async () => {
-		const fileName = await pathToFileName('/path/to/test.txt');
+		const fileName = pathToFileName('/path/to/test.txt');
 		expect(fileName).toBe('test.txt');
 	});
 
 	it('will work with a \\path\\to\\filename (backslashes)', async () => {
-		const fileName = await pathToFileName('\\path\\to\\test.txt');
+		const fileName = pathToFileName('\\path\\to\\test.txt');
 		expect(fileName).toBe('test.txt');
 	});
 
 	it('will work from a drive letter', async () => {
-		const fileName = await pathToFileName('C:\\path\\to\\test.txt');
+		const fileName = pathToFileName('C:\\path\\to\\test.txt');
 		expect(fileName).toBe('test.txt');
 	});
 
 	it('will work with a /path/to/filename with no extension', async () => {
-		const fileName = await pathToFileName('/path/to/test');
+		const fileName = pathToFileName('/path/to/test');
 		expect(fileName).toBe('test');
 	});
 
 	it('will throw an error if the path is undefined', async () => {
-		// @ts-expect-error
-		await expect(pathToFileName(undefined)).rejects.toThrow();
+		expect(() => {
+			// @ts-expect-error
+			pathToFileName(undefined);
+		}).toThrow();
+	});
+
+	it('will not parse slashes as a filename', async () => {
+		expect(() => pathToFileName('\\')).toThrow();
 	});
 });
--- src/utils/pathToFileName/index.ts
+++ src/utils/pathToFileName/index.ts
@@ -1,6 +1,7 @@
 import fs from 'fs-extra';
 import path, { extname } from 'path';
 
+import { assertType } from '~/utils/assertType';
 import { throwError } from '~/utils/throwError/index';
 
 /**
@@ -9,8 +10,11 @@ import { throwError } from '~/utils/throwError/index';
  * If the path provided has an extension, it will be created as a file.
  */
 
-export async function pathToFileName(path: string): Promise<string> {
+export function pathToFileName(path: string): string {
+	assertType({ path }, 'string');
 	const fileName = path.replaceAll('\\', '/').split('/').pop();
+
+	// This error will only be thrown if we are passed in a single slash
 	if (!fileName) throwError('fileName passed in turned into undefined. Path: ', path);
 
 	return fileName;
