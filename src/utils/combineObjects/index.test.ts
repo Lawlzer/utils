4c7a02d377d6bf7c4f8ac19cc0052ee536b279c8
Lawlzer <Kevindaspam@gmail.com>
2022-12-02 16:00:54 -0600

    ✅Update tests for combineObjects

--- src/utils/combineObjects/index.test.ts
+++ src/utils/combineObjects/index.test.ts
@@ -9,19 +9,19 @@ describe(folderName, () => {
 		expect(result).toEqual({ a: 'a', b: 'b' });
 	});
 
-	it('will work with empty objects', () => {
-		const obj1 = { a: 'a' };
-		const obj2 = {};
-		const result = combineObjects(obj1, obj2);
-		expect(result).toEqual({ a: 'a' });
-	});
-
-	it('will combine multiple (3+) objects with different properties', () => {
+	it('will combine 3 objects with different properties', () => {
 		const obj1 = { a: 'a' };
 		const obj2 = { b: 'b' };
 		const obj3 = { c: 'c' };
 		const result = combineObjects(obj1, obj2, obj3);
 		expect(result).toEqual({ a: 'a', b: 'b', c: 'c' });
+	});
+
+	it('will combine an empty object with a normal object', () => {
+		const obj1 = { a: 'a' };
+		const obj2 = {};
+		const result = combineObjects(obj1, obj2);
+		expect(result).toEqual({ a: 'a' });
 	});
 
 	it('will add numbers', () => {
