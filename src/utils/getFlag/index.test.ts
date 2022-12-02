c29f358ff978fb26546afd44e621262a87be6e99
Lawlzer <Kevindaspam@gmail.com>
2022-12-02 15:58:25 -0600

    🚩Update getFlag to check for ENV vars

--- src/utils/getFlag/index.test.ts
+++ src/utils/getFlag/index.test.ts
@@ -5,33 +5,40 @@ import { getFlag } from './index';
 const folderName = __dirname.split('\\').pop()!;
 
 describe(folderName, () => {
+	// Resetting the process.argv and process.env this way will actually modify how they work!
+	// As an example, process.env capitalization does not matter. If you reset it to {}, capitalization does matter!
+	// beforeEach(() => {
+	// 	process.argv = [];
+	// 	process.env = {};
+	// });
+
 	it('will parse --flag as true', () => {
-		process.argv[2] = '--flag';
-		const flag = getFlag('flag');
+		process.argv[2] = '--test1';
+		const flag = getFlag('test1');
 		expect(flag).toBe(true);
 	});
 
 	it('will parse -flag as true', () => {
-		process.argv[2] = '-flag';
-		const flag = getFlag('flag');
+		process.argv[2] = '-test2';
+		const flag = getFlag('test2');
 		expect(flag).toBe(true);
 	});
 
 	it('will parse "--flag=value" as value', () => {
-		process.argv[2] = '--flag=value';
-		const flag = getFlag('flag');
+		process.argv[2] = '--test3=value';
+		const flag = getFlag('test3');
 		expect(flag).toBe('value');
 	});
 
 	it('will parse "-flag=value" as value', () => {
-		process.argv[2] = '-flag=value';
-		const flag = getFlag('flag');
+		process.argv[2] = '-test4=value';
+		const flag = getFlag('test4');
 		expect(flag).toBe('value');
 	});
 
 	it('will parse "flag" as true', () => {
-		process.argv[2] = 'flag';
-		const flag = getFlag('flag');
+		process.argv[2] = 'test5';
+		const flag = getFlag('test5');
 		expect(flag).toBe(true);
 	});
 
@@ -41,35 +48,35 @@ describe(folderName, () => {
 	});
 
 	it('will not parse a number', () => {
-		process.argv[2] = '--flag=12';
-		const flag = getFlag('flag');
+		process.argv[2] = '--test6=12';
+		const flag = getFlag('test6');
 		expect(flag).toBe('12');
 	});
 
 	it('will parse --flag=true as true', () => {
-		process.argv[2] = '--flag=true';
-		const flag = getFlag('flag');
+		process.argv[2] = '--test7=true';
+		const flag = getFlag('test7');
 		expect(flag).toBe(true);
 	});
 
 	it('will parse --flag=false as false', () => {
-		process.argv[2] = '--flag=false';
-		const flag = getFlag('flag');
+		process.argv[2] = '--test8=false';
+		const flag = getFlag('test8');
 		expect(flag).toBe(false);
 	});
 
 	it('will parse flags with dashes', () => {
-		process.argv[2] = '--flag-with-dashes=foo';
-		const flag = getFlag('flag-with-dashes');
+		process.argv[2] = '--flag-with-dashes1=foo';
+		const flag = getFlag('flag-with-dashes1');
 		expect(flag).toBe('foo');
 	});
 
-	it('will not incorrectly parse a flag with dashes', () => {
-		process.argv[2] = '--flag-with-dashes';
-		const flag1 = getFlag('flag');
+	it('will not mis-parse part of a flag', () => {
+		process.argv[2] = '--flag9-with-dashes2';
+		const flag1 = getFlag('flag9');
 		expect(flag1).toBeUndefined();
 
-		const flag2 = getFlag('with');
+		const flag2 = getFlag('flag9-with');
 		expect(flag2).toBeUndefined();
 
 		const flag3 = getFlag('dashes');
@@ -78,17 +85,47 @@ describe(folderName, () => {
 
 	it('will not incorrectly parse a flag that starts with the string', () => {
 		process.argv[2] = '--flagTricked';
-		const flag1 = getFlag('flag');
+		const flag1 = getFlag('flag10');
 		expect(flag1).toBeUndefined();
 	});
 
 	it('will parse multiple flags', () => {
-		process.argv[2] = '--flag1=foo';
-		process.argv[3] = '--flag2=bar';
-		const flag1 = getFlag('flag1');
+		process.argv[2] = '--flag11=foo';
+		process.argv[3] = '--flag12=bar';
+		const flag1 = getFlag('flag11');
 		expect(flag1).toBe('foo');
 
-		const flag2 = getFlag('flag2');
+		const flag2 = getFlag('flag12');
 		expect(flag2).toBe('bar');
+	});
+
+	it('will parse differently capitalized flags', () => {
+		process.argv[2] = '--flAG13=foo';
+		const flag = getFlag('fLag13');
+		expect(flag).toBe('foo');
+	});
+
+	it('will not lowercase the flag value', () => {
+		process.argv[2] = '--flag14=FOO';
+		const flag = getFlag('flag14');
+		expect(flag).toBe('FOO');
+	});
+
+	it('will parse env variables', () => {
+		process.env.flag15 = 'foo';
+		const flag = getFlag('flag15');
+		expect(flag).toBe('foo');
+	});
+
+	it('will parse env variables with dashes', () => {
+		process.env['FLAG16-WITH-DASHES'] = 'foo';
+		const flag = getFlag('FLAG16-WITH-DASHES');
+		expect(flag).toBe('foo');
+	});
+
+	it('will parse env variables with different capitalization', () => {
+		process.env.FLag17 = 'foo';
+		const flag = getFlag('flAg17');
+		expect(flag).toBe('foo');
 	});
 });
--- src/utils/getFlag/index.ts
+++ src/utils/getFlag/index.ts
@@ -4,31 +4,52 @@ import { throwError } from '~/utils/throwError';
  * Find a CLI flag, and return the result.
  *
  * Will always return a string, or undefined. WILL ONLY PARSE BOOLEANS, will NOT parse --value=12.
+ *
+ * Will also read process.env variables.
  */
 export function getFlag(flagInput: string): string | boolean | undefined {
+	const flagLowercase = flagInput.toLowerCase();
 	// The flags are not truly "flags", they are simple argv.  For example, "--foo=foo" will be "--foo=foo" as an argv.
 	// So we will find the relevant flag (either find "foo", "-foo", or "--foo"), and return the value (or, if no value, true)
 
-	// --foo -> true
-	// --foo=foo -> foo
-	// -foo=foo -> foo
-	// foo -> true
-	// foo=false -> false
-	// foo-true = true
+	// Handle .env variables
+	// .env variables are always lowercase, so capitalization does not matter whatsoever.
+	const envValue = process.env[flagLowercase];
+	if (envValue !== undefined) return envValue;
 
-	// --flag | -flag | flag
-	const flagSimple = process.argv.find((arg) => arg === flagInput || arg === `-${flagInput}` || arg === `--${flagInput}`);
-	if (flagSimple) return true;
+	// Handle process.argv (CLI) variables
+	// Find the flag we are referencing here
+	let flag = process.argv.find((argv) => {
+		if (!argv) return false; // if the argv have been deleted, or set to null/undefined
 
-	// --flag=value | -flag=value || flag=value
-	const flagWhole = process.argv.find((arg) => arg.startsWith(`--${flagInput}=`) || arg.startsWith(`-${flagInput}=`) || arg.startsWith(`${flagInput}=`));
-	if (!flagWhole) return undefined; // flag not found
+		let arg = argv.toLowerCase();
+		if (arg.startsWith('-')) arg = arg.replace('-', '');
+		if (arg.startsWith('-')) arg = arg.replace('-', '');
 
-	const flagValue = flagWhole.split(flagInput + '=')[1];
-	if (!flagValue) return true; // --flag
+		if (arg.toLowerCase().startsWith(`${flagLowercase}=`) || arg === flagLowercase) return true;
+		return false;
+	});
+	console.log('flag: ', flag);
+	if (!flag) return undefined;
 
-	if (flagValue === 'true') return true; // --flag=true
-	if (flagValue === 'false') return false; // --flag=false
+	// flagValue should be everything after the first =
+	// If there is no =, it should be true
+	// We should assume there may be multiple =
+	// For example, --foo=bar=baz should be bar=baz
+	if (flag === `${flagLowercase}`) return true; // --foo
+	if (flag === `-${flagLowercase}`) return true; // -foo
+	if (flag === `--${flagLowercase}`) return true; // --foo
 
-	return flagValue ? flagValue : true;
+	const equalsIndex = flag.indexOf('=');
+
+	const flagValue = flag.slice(equalsIndex + 1);
+
+	if (flagValue === 'true') return true;
+	if (flagValue === 'false') return false;
+
+	return flagValue;
 }
+
+process.argv[2] = '-test2';
+const flag = getFlag('test2');
+console.log(flag);
